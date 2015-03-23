d31676792b77a5e76998f0d358ca0a0195e00e4a
Deekshith Allamaneni <dkh.hyd@gmail.com>
2015-03-23 02:42:48 -0500

    Captures screenshot of rectangle and also extracts text.

--- screen-scrape.py
+++ screen-scrape.py
@@ -1,8 +1,19 @@
 import pyscreenshot as ImageGrab
-from StringIO import StringIO
+import Image
+import pytesseract
 
 def captureScreenArea(pos_x, pos_y, length_x, length_y):
 	im=ImageGrab.grab(bbox=(pos_x, pos_y, pos_x+length_x, pos_y+length_y)) # X1,Y1,X2,Y2
-	im.show()
+	return im
 
-captureScreenArea(10,10,100,100)
+if __name__ == "__main__":
+	capturedIm = captureScreenArea(580,150,200,50)
+	extractedText = pytesseract.image_to_string(capturedIm)
+	print extractedText
+	if "anllne" in extractedText:
+		print "Is online"
+	capturedIm.show()
+
+
+# print pytesseract.image_to_string(capturedIm)
+# capturedIm.show()
